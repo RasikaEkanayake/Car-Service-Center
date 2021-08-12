5b0b1d406dd8ea9da547de77d934fe6bc9782deb
DELL <sachindileepa41@gmail.com>
2021-08-13 00:04:45 +0530

    Suppliermanagement

---
+++ SupplierManagement/Supplierform/Supplierform/Supplierform.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Supplierform
+{
+    partial class Supplierform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lab1sup = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lab1sup
+            // 
+            this.lab1sup.AutoSize = true;
+            this.lab1sup.Location = new System.Drawing.Point(495, 9);
+            this.lab1sup.Name = "lab1sup";
+            this.lab1sup.Size = new System.Drawing.Size(146, 17);
+            this.lab1sup.TabIndex = 0;
+            this.lab1sup.Text = "Supplier Management";
+            this.lab1sup.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Supplierform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(1138, 602);
+            this.Controls.Add(this.lab1sup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Supplierform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.supplier_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab1sup;
+    }
+}
+
---
+++ SupplierManagement/Suppliermanage/Supplierform.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suppliermanage
+{
+    public partial class Supplierform : Form
+    {
+        public Supplierform()
+        {
+            InitializeComponent();
+        }
+
+        private void suppliermanage_Load(object sender, EventArgs e)
+        {
+            int w = Screen.PrimaryScreen.Bounds.Width;
+            int h = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0, 0);
+            this.Size = new Size(w, h);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
