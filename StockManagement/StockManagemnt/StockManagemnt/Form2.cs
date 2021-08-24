56f7a9aa28aad5c4dc34286886cf8e3815e5fd3a
Dileshakdn <89330340+Dileshakdn@users.noreply.github.com>
2021-08-24 17:03:51 +0530

    Interfaces

---
+++ StockManagement/StockManagemnt/StockManagemnt/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockManagemnt
+{
+    public partial class Form2 : Form
+    {
+        //fix rectangle to curve
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+             (
+                 int nLeft,
+                 int nTop,
+                 int nRight,
+                 int nBottom,
+                 int nwidthEllipse,
+                 int nHightEllipse
+             );
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //button1 border changing to curve
+            button1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, button1.Width, button1.Height, 15, 15));
+            button2.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, button1.Width, button1.Height, 15, 15));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+
+        }
+    }
+}
